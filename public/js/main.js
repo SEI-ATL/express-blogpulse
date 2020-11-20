e407bb6c36c151ebe1c20b9845d6bde61e9a8200
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-20 00:37:03 -0500

    Add faux modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,21 @@
 $('#myModal').on('shown.bs.modal', function() {
     $('#myInput').focus()
 })
+
+//I don't realyl understadn this jQuery and it's getting too late, I might come back to this
+/*
+$('#save').click(function() {
+    $('#someModalId').modal('hide');
+    $(document).ready(function() {
+        $.ajax({
+            method: "POST",
+            url: "/articles/new",
+            data: {
+                num: num,
+                text: $("#message-text").val()
+            }
+        })
+
+    })
+})
+*/
