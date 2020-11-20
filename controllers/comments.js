05b7a6ab3e867e8650de110b075a6ca553a2db1c
jtreeves <jackson.t.reeves@gmail.com>
2020-11-20 00:05:21 -0500

    [ejs] Create controller for comments routers, and add to ejs files for adding and displaying comments

---
+++ controllers/comments.js
@@ -0,0 +1,31 @@
+let express = require('express')
+let db = require('../models')
+let router = express.Router()
+
+// POST /comments - create a new comment
+router.post('/', (req, res) => {
+    db.comment.create({
+        name: req.body.name,
+        content: req.body.content,
+        articleId: req.body.articleId
+    })
+    .then((post) => {
+        res.redirect('/')
+    })
+    .catch((error) => {
+        res.status(400).render('main/404')
+    })
+})
+  
+// GET /comments/new - display form for creating new comments
+router.get('/new', (req, res) => {
+    db.comment.findAll()
+    .then((articles) => {
+        res.render('comments/new', { articles: articles })
+    })
+    .catch((error) => {
+        res.status(400).render('main/404')
+    })
+})  
+
+module.exports = router
