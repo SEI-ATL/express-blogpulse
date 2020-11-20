031eb10cc4952852a377089f116b19d02925c3f7
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-20 10:06:30 -0500

    better associations

--- dbTest.js
+++ dbTest.js
@@ -12,14 +12,14 @@
 
 // dbTest()
 
-// const db = require('./models')
+const db = require('./models')
 
-// async function test() {
-//   const article = await db.article.findOne({
-//     where: { id: 1 },
-//     include: [db.comment]
-//   })
-//   // by using eager loading, the article model should have a comments key
-//   console.log(article.comments)
-// }
-// test()
+async function test() {
+  const article = await db.article.findOne({
+    where: { id: 1 },
+    include: [db.comment]
+  })
+  // by using eager loading, the article model should have a comments key
+  console.log(article.comments)
+}
+test()
