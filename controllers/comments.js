9989a8280b4b994a8191a7a8a7e588f75bcf6fae
jtreeves <jackson.t.reeves@gmail.com>
2020-11-20 00:44:43 -0500

    [js] Reconfigure how comments work with the site

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,14 +3,14 @@ let db = require('../models')
 let router = express.Router()
 
 // POST /comments - create a new comment
-router.post('/', (req, res) => {
+router.post('/articles/:id/comments', (req, res) => {
     db.comment.create({
         name: req.body.name,
         content: req.body.content,
         articleId: req.body.articleId
     })
     .then((post) => {
-        res.redirect('/')
+        res.redirect('/articles')
     })
     .catch((error) => {
         res.status(400).render('main/404')
@@ -18,14 +18,14 @@ router.post('/', (req, res) => {
 })
   
 // GET /comments/new - display form for creating new comments
-router.get('/new', (req, res) => {
-    db.comment.findAll()
-    .then((articles) => {
-        res.render('comments/new', { articles: articles })
-    })
-    .catch((error) => {
-        res.status(400).render('main/404')
-    })
-})  
+// router.get('/new', (req, res) => {
+//     db.comment.findAll()
+//     .then((articles) => {
+//         res.render('comments/new', { articles: articles })
+//     })
+//     .catch((error) => {
+//         res.status(400).render('main/404')
+//     })
+// })  
 
 module.exports = router
